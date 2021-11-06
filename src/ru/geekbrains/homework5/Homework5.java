f96e2b088f780ea316f5bd17e0c082512442e564
KseniyaZakh <ksenzakharova@gmail.com>
2021-11-06 15:59:00 +0300

    Part 1

---
+++ src/ru/geekbrains/homework5/Homework5.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.homework5;
+
+import java.util.Arrays;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        addArray1();
+    }
+
+    public static void addArray1() {
+        int size = 10000000;
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1.0f;
+        }
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println("Время работы метода1: " + (System.currentTimeMillis() - start));
+    }
+}
+
+
+
